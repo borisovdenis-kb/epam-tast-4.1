1bc1c0d839276a687d6a80267b75f42ad2254684
Denis Borisov <borisovdenis-kb@yandex.ru>
2017-10-16 21:22:12 +0400

    Начал исправлять косяки. Подчистил pom. Убрал выравнивание по ширине в WorkDemonstration.

--- src/main/java/ru/intodayer/InjectorWorkDemonstration.java
+++ src/main/java/ru/intodayer/InjectorWorkDemonstration.java
@@ -7,20 +7,20 @@ import ru.intodayer.humans.Fellow;
 
 public class InjectorWorkDemonstration {
     public static void demonstrateInjectorWork() {
-        Fellow fellow       = new Fellow();
-        Cache[] caches      = CacheUtility.createCaches();
-        Injector injector   = new Injector(caches);
+        Fellow fellow = new Fellow();
+        Cache[] caches = CacheUtility.createCaches();
+        Injector injector = new Injector(caches);
 
         injector.inject(fellow);
 
         System.out.println(
-            "Fellow`s ex-girlfriends:\n" + fellow.getExGirlFriendNames() + "\n"
+                "Fellow`s ex-girlfriends:\n" + fellow.getExGirlFriendNames() + "\n"
         );
         System.out.println(
-            "Fellow`s achievements <private field from SUPER>:\n" + fellow.getAchievements() + "\n"
+                "Fellow`s achievements <private field from SUPER>:\n" + fellow.getAchievements() + "\n"
         );
         System.out.println(
-            "Fellow`s brilliant ideas <private field from SUPER.SUPER>:\n" + fellow.getBrilliantIdeas() + "\n"
+                "Fellow`s brilliant ideas <private field from SUPER.SUPER>:\n" + fellow.getBrilliantIdeas() + "\n"
         );
     }
 }
